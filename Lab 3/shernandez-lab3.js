9d1d692819506f108779201ec328f0a588c06d58
shernandez13 <shernandez13@hawk.iit.edu>
2025-03-05 11:40:00 -0600

    Update shernandez-lab3.js

--- Lab 3/shernandez-lab3.js
+++ Lab 3/shernandez-lab3.js
@@ -1,3 +1,15 @@
 // Stephanie Hernandez
 // ITMD 541-02 Graduate Student
 
+//Exercise #1
+
+//This part will look at the numbers of the array and analyze them
+function minMaxAverage(numbers) {
+    let totalNumbers = numbers.length;
+    let minValue = Math.min(...numbers);
+    let maxValue = Math.max(...numbers);
+    let average = numbers.reduce((sum,num) => sum + num, 0) / totalNumbers;
+
+    console.log('Total Numbers: ${total Numbers}, Min Value: ${minValue}, Max Value: ${maxValue}, Average: ${average.toFixed(2)}');
+ // It will find the amount of numbers, smallest value, largest value, and average value   
+}
